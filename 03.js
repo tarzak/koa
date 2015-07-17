debadcf4e9a7a7cff22a23b9fd8239b4f4136d16
tarzak <jobtarzak@gmail.com>
2015-07-17 18:10:37 +0300

    rm bodyparsers

--- 03.js
+++ 03.js
@@ -1,18 +1,8 @@
 var express    = require('express')
-var bodyParser = require('body-parser')
 var fs = require('fs');
 var path = require('path');
 var app = express();
 var stream = fs.createWriteStream('public/file.txt');
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'), function (err) {
