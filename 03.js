06c6b0b819d33657f1e518e51b2d7d5c530e0254
tarzak <jobtarzak@gmail.com>
2015-09-25 15:33:34 +0300

    rm two end()

--- 03.js
+++ 03.js
@@ -17,7 +17,7 @@ app.get('/file', function (req, res) {
 app.put('/file', function (req, res) {
   pipe([req, fs.createWriteStream(filePath, {'flags': 'a'})], function (error) {
     if (error)
-      res.status(500).send(error.message).end();
+      res.status(500).send(error.message);
     else
       res.send('success')
   });
@@ -27,7 +27,7 @@ function sendFileWithPipe(filePath, responseStream) {
   var readStream = fs.createReadStream(filePath);
   pipe([readStream, responseStream], function (error) {
     if (error)
-      responseStream.send(error.message).end();
+      responseStream.send(error.message);
   });
 }
 
