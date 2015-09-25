95f08f8b111fcb06dd010bedc66a50eecc6752c0
tarzak <jobtarzak@gmail.com>
2015-09-25 13:51:55 +0300

    made getIndex and getFile with common function

--- 03.js
+++ 03.js
@@ -2,40 +2,38 @@ var express    = require('express'),
     fs   = require('fs'),
     path = require('path'),
     app  = express(),
-    file = 'public/file.txt',
+    indexPath = 'public/index.html',
+    filePath = 'public/file.txt',
     pipe = require('pipe-io');
 
 app.get('/', function(req, res) {
-  var readStream = fs.createReadStream('public/index.html');
-  pipe([readStream, res], function (error) {
-    console.log(error || 'sent');
-    if (error)
-      res.status(error.status).end();
-    else
-      res.end();
-  });
+  sendFileWithPipe(indexPath, res);
 });
 
 app.get('/file', function (req, res) {
-  var readStream = fs.createReadStream(file);
-  pipe([readStream, res], function (error) {
-    console.log(error || 'done');
-    if (error)
-      res.status(error.status).end();
-    else
-      res.end();
-  });
+  sendFileWithPipe(filePath, res);
 });
 
 app.put('/file', function (req, res) {
   pipe([req, fs.createWriteStream(file, {'flags': 'a'})], function (error) {
     console.log(error || 'done');
     if (error)
-      res.status(error.status).end();
+      res.status(500).end();
     else
       res.end();
   });
 });
+
+function sendFileWithPipe(filePath, responseStream) {
+  var readStream = fs.createReadStream(filePath);
+  pipe([readStream, responseStream], function (error) {
+    console.log(error || 'file ' + filePath + ' is sent');
+    if (error)
+      responseStream.status(500).end();
+    else
+      responseStream.end();
+  });
+}
 
 console.log('Server is listening 3000 port. Enjoy :).')
 app.listen(3000);
